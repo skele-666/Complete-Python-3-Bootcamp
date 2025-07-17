9e5f3cafdffd2ee98a4ca3c088fe07e3337940d1
Ella Jozinovic <ellajozi01@gmail.com>
2025-07-17 20:25:47 +0930

    Add my version of module/package notes

---
+++ 06-Modules and Packages/my version/Package-Module Example/myprogram.py
@@ -0,0 +1,13 @@
+# Importing from package
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mysubscript
+
+some_main_script.report_main()
+mysubscript.sub_report()
+
+
+
+# Improrting from module
+from mymodule import my_func
+
+my_func()
---
+++ 06-Modules and Packages/my version/my version/one.py
@@ -0,0 +1,24 @@
+#one.py
+# Any code at indendation level 0 will be executed when this file is run.
+# There is no main function in python. But implicitly, all code at indentation level 0 is executed
+
+# If the script is ran directly, __name__ will be set to "__main__"
+# So, you can check with an if statement (usually placed at the end):
+# if __name__ == '__main__':
+#     print('This script is being run directly')
+
+def func():
+    print("FUNC IN ONE.PY")
+
+def function():
+    pass
+
+def function2():
+    pass
+
+print("TOP LEVEL IN ONE.PY")
+
+if __name__ == '__main__':
+    # RUN THE SCRIPT!
+    function2()
+    function()
